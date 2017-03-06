0986ea8a09211b57f29893b82386a77c81858a6f
Leta Keane <letakeane@users.noreply.github.com>
2017-03-05 21:15:32 -0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-$("a").on('click', function() {
-  var hash = this.hash;
-  $('html, body').animate({
-    scrollTop: $(hash).offset().top
-  }, 800, function(){
-    window.location.hash = hash;
-  });
-});
